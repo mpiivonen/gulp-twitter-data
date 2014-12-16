918aef6de28d081c4e539dc7ccde70fbb8891291
Markus Iivonen <markus.iivonen@metropolia.fi>
2014-12-16 16:52:06 +0200

    added tag

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,6 @@ var fileTracker = require('./index.js');
 var file = 'testfile.log',
     maxSize = '50 kB',
     location = './';
-
 gulp.task('default', function () {
   fileTracker(file,maxSize,location);
 });
