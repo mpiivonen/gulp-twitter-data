2599bd0e0f9b1c5699f094bdc75af08c60f09421
Markus Iivonen <markus.iivonen@metropolia.fi>
2014-12-19 14:29:19 +0200

    test structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,9 @@
 var gulp = require('gulp');
-var fileTracker = require('./index.js');
+var stream = require('./index.js');
 
 var file = 'testfile.log',
     maxSize = '50 kB',
     location = './';
 gulp.task('default', function () {
-  fileTracker(file,maxSize,location);
+  stream();
 });
--- index.js
+++ index.js
@@ -1,60 +1,12 @@
 var fs = require('fs'),
     fse = require('fs.extra'),
-    prettyBytes = require('pretty-bytes');
+    prettyBytes = require('pretty-bytes'),
+    twitter = require('twitter-data');
 
 const PLUGIN_NAME = 'gulp-twitter-data';
-
-// Return a string containing file size and unit e.g 1 kB
-function getFileSize(file,cb) {
-  fs.stat(file, function (err, stats) {
-
-    return cb(prettyBytes(stats.size));
-  });
-}
-
-// Return true if file size is larger than maxSize, otherwise false
-function compareFileSize(file,maxSize,cb) {
-
-  var units = ["B", "kB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"  ];
-  getFileSize(file,function(size){
-
-    var fileUnit = size.split(" ");
-    var maxUnit = maxSize.split(" ");
-
-    if(units.indexOf(fileUnit[1]) < units.indexOf(maxUnit[1])) {
-      return cb(0);
-    }
-    else if(units.indexOf(fileUnit[1]) === units.indexOf(maxUnit[1]) && fileUnit[0] <= maxUnit[0]) {
-      return cb(0);
-    }
-    else{
-      return cb(1);
-    }
-  });
-
-}
 
 module.exports = function(file,maxSize,newLocation)
 {
 
-  var logNumber = 1;
-  fs.watchFile(file, function (cb) {
-    console.log("File size changed");
-    compareFileSize(file,maxSize,function(res){
-      if(res){
-        console.log("File size exceeded");
-        fse.copy(file, newLocation+file+'.'+logNumber, function (err) {
-          if (err) {
-            throw err;
-          }
-          logNumber++;
-          fs.truncate(file, 0, function(){console.log('log copied')})
-        });
-      }
-      else {
-        console.log("File size under desired size");
-      }
-    });
-  });
 
 };
